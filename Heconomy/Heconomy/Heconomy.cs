35e74b86d4702826bd2ec632c1e9cfa5ef44fa6e
Herb9 <pksxtm@gmail.com>
2017-02-13 21:01:31 +0900

    Initial commit.

---
+++ Heconomy/Heconomy/Heconomy.cs
@@ -0,0 +1,26 @@
+﻿/*
+    ooooo   ooooo                     .o8        .ooooo.   
+    `888'   `888'                    "888       888' `Y88. 
+     888     888   .ooooo.  oooo d8b  888oooo.  888    888 
+     888ooooo888  d88' `88b `888""8P  d88' `88b  `Vbood888 
+     888     888  888ooo888  888      888   888       888' 
+     888     888  888    .o  888      888   888     .88P'  
+    o888o   o888o `Y8bod8P' d888b     `Y8bod8P'   .oP'     
+
+    Directed by Herb9.
+*/
+
+namespace Heconomy
+{
+
+    public class Heconomy
+    {
+
+        public const string Prefix = "\x5b\x48\x65\x63\x6f\x6e\x6f\x6d\x79\x5d";
+
+        public Heconomy()
+        {
+
+        }
+    }
+}
---
+++ Heconomy/Heconomy/PluginLoader.cs
@@ -0,0 +1,50 @@
+﻿
+/*
+    ooooo   ooooo                     .o8        .ooooo.   
+    `888'   `888'                    "888       888' `Y88. 
+     888     888   .ooooo.  oooo d8b  888oooo.  888    888 
+     888ooooo888  d88' `88b `888""8P  d88' `88b  `Vbood888 
+     888     888  888ooo888  888      888   888       888' 
+     888     888  888    .o  888      888   888     .88P'  
+    o888o   o888o `Y8bod8P' d888b     `Y8bod8P'   .oP'     
+
+    Directed by Herb9.
+*/
+
+using MiNET.Plugins;
+using MiNET.Plugins.Attributes;
+
+namespace Heconomy
+{
+
+    [Plugin(
+        PluginName = "HerbTeleportRequest",
+        Description = "Allows regular users to teleport to and summon based on an acceptance of the other user.",
+        PluginVersion = "1.0",
+        Author = "Herb9"
+        )]
+    public class PluginLoader : Plugin
+    {
+
+        private string Prefix;
+
+        protected override void OnEnable()
+        {
+            Prefix = Heconomy.Prefix;
+
+            CheckUpdate();
+
+            RegisterCommands();
+        }
+
+        private void CheckUpdate()
+        {
+
+        }
+
+        private void RegisterCommands()
+        {
+
+        }
+    }
+}
