94ba84425283915698cdc25910fcbd01a65604aa
Herb9 <pksxtm@gmail.com>
2017-02-20 10:05:48 +0900

    Replaced ascii name comment and sorted using part.

--- HeconomyAPI/HeconomyAPI/HeconomyAPI.cs
+++ HeconomyAPI/HeconomyAPI/HeconomyAPI.cs
@@ -1,13 +1,12 @@
 ﻿
 /*
-    888    888                  888       .d8888b.  
-    888    888                  888      d88P  Y88b 
-    888    888                  888      888    888 
-    8888888888  .d88b.  888d888 88888b.  Y88b. d888 
-    888    888 d8P  Y8b 888P"   888 "88b  "Y888P888 
-    888    888 88888888 888     888  888        888 
-    888    888 Y8b.     888     888 d88P Y88b  d88P 
-    888    888  "Y8888  888     88888P"   "Y8888P"   
+    ooooo   ooooo                     .o8        .ooooo.   
+    `888'   `888'                    "888       888' `Y88. 
+     888     888   .ooooo.  oooo d8b  888oooo.  888    888 
+     888ooooo888  d88' `88b `888""8P  d88' `88b  `Vbood888 
+     888     888  888ooo888  888      888   888       888' 
+     888     888  888    .o  888      888   888     .88P'  
+    o888o   o888o `Y8bod8P' d888b     `Y8bod8P'   .oP'     
 
     Directed by Herb9.
 */
@@ -15,8 +14,8 @@
 using MiNET;
 using MiNET.Worlds;
 
-using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 using System;
 using System.IO;
@@ -67,14 +66,13 @@ namespace HeconomyAPI
         }
 
         /*
-                   d8888 8888888b. 8888888 
-                  d88888 888   Y88b  888   
-                 d88P888 888    888  888   
-                d88P 888 888   d88P  888   
-               d88P  888 8888888P"   888   
-              d88P   888 888         888   
-             d8888888888 888         888   
-            d88P     888 888       8888888 
+                  .o.       ooooooooo.   ooooo 
+                 .888.      `888   `Y88. `888' 
+                .8"888.      888   .d88'  888  
+               .8' `888.     888ooo88P'   888  
+              .88ooo8888.    888          888  
+             .8'     `888.   888          888  
+            o88o     o8888o o888o        o888o 
 
             You can using api functions.
         */
